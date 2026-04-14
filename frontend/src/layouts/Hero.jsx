20e131865b6c14c73c85cb10b52960d764f18a49
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2026-04-14 20:35:25 +0300

    Added a home button to the landing page in case user is authorized

--- frontend/src/layouts/Hero.jsx
+++ frontend/src/layouts/Hero.jsx
@@ -1,20 +1,32 @@
 import HeroText from "../components/HeroText.jsx"
-import WithBackgroundBtn from "../components/WithBackgroundBtn.jsx";
-import {Link} from "react-router-dom";
-import NoBackgroundBtn from "../components/NoBackgroundBtn.jsx";
+import WithBackgroundBtn from "../components/WithBackgroundBtn.jsx"
+import { Link } from "react-router-dom"
+import NoBackgroundBtn from "../components/NoBackgroundBtn.jsx"
+import { useAuth } from "../context/AuthContext.jsx"
 
 function Hero() {
+    const { user } = useAuth()
+
     return (
         <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-20 pt-16 pb-24 text-center">
-                <HeroText />
-            <div className="flex sm:flex-row items-center justify-center gap-5 pt-8">
-                <Link to="/register">
-                    <WithBackgroundBtn text="Reģistrēties"/>
-                </Link>
+            <HeroText />
 
-                <Link to="/login">
-                    <NoBackgroundBtn text="Pieslēgties"/>
-                </Link>
+            <div className="flex sm:flex-row items-center justify-center gap-5 pt-8">
+                {user ? (
+                    <Link to="/home">
+                        <NoBackgroundBtn text="Uz sākumu" />
+                    </Link>
+                ) : (
+                    <>
+                        <Link to="/register">
+                            <WithBackgroundBtn text="Reģistrēties"/>
+                        </Link>
+
+                        <Link to="/login">
+                            <NoBackgroundBtn text="Pieslēgties"/>
+                        </Link>
+                    </>
+                )}
             </div>
         </div>
     )
